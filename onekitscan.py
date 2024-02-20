da81c90a9b6f1b7c1d71c3f423bfd1cc0184d0e8
jaanavi_jrv <jaanavi.rv@intel.com>
2024-02-20 10:04:18 +0530

    Added scan status api

--- onekitscan.py
+++ onekitscan.py
@@ -3,7 +3,7 @@ import sys
 
 file_url = sys.argv[1]
 
-url = "http://onekitscan.idoc.intel.com/api/v1/scan"
+post_url = "http://onekitscan.idoc.intel.com/api/v1/scan"
 
 body = { "submitted_user": "jrv", "reference_id": "1234", "file_url": file_url }
 
@@ -11,4 +11,12 @@ post_response = requests.post(url, json=body)
 
 post_response_json = post_response.json()
 
-print(post_response_json)
+print("Response after uploading to BDBA server: ", post_response_json)
+
+scan_id = post_response_json["id"]
+
+get_url = f"http://onekitscan.idoc.intel.com/api/v1/scan?id={scan_id}"
+
+post_response = requests.get(get_url)
+
+print("Status of scan: ",post_response)

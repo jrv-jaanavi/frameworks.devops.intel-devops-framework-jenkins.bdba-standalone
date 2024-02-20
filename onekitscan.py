54709d7cd020991dafac52d5900e3f4f17580f2e
jaanavi_jrv <jaanavi.rv@intel.com>
2024-02-20 10:07:16 +0530

    Fixing post request

--- onekitscan.py
+++ onekitscan.py
@@ -7,7 +7,7 @@ post_url = "http://onekitscan.idoc.intel.com/api/v1/scan"
 
 body = { "submitted_user": "jrv", "reference_id": "1234", "file_url": file_url }
 
-post_response = requests.post(url, json=body)
+post_response = requests.post(post_url, json=body)
 
 post_response_json = post_response.json()
 

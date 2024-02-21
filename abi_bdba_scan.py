92f44a4d12be3c60b9b6ddcbbe471576ae0cf433
jaanavi_jrv <jaanavi.rv@intel.com>
2024-02-21 14:28:45 +0530

    pkg name get from user

--- abi_bdba_scan.py
+++ abi_bdba_scan.py
@@ -19,5 +19,5 @@ pip_cmd = "pip install --upgrade pip && pip install  abi==3.0.0 --extra-index-ur
 pip_cmd_output = subprocess.run(pip_cmd, capture_output=True, text=True)
 
 pkg_name = sys.argv[2]
-bdba_scan_cmd = f"abi binary_scan scan --timeout 40 --wait --tool_url "https://bdba001.icloud.intel.com" --tool_group "6"  --report_name {pkg_name} --include_components  --zip_file SampleWin_1.0.00.7349.zip --username "%username%" --password "%password%" --debug "
+bdba_scan_cmd = f"abi binary_scan scan --timeout 40 --wait --tool_url "https://bdba001.icloud.intel.com" --tool_group "6"  --report_name "BDBA_Report" --include_components  --zip_file {pkg_name} --username {scan_username} --password {scan_password} --debug "
 bdba_scan_cmd_output = subprocess.run(bdba_scan_cmd, capture_output=True, text=True)

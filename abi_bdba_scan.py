daa5cdf993193026d4ddf59a02ec774344c6eb2d
jaanavi_jrv <jaanavi.rv@intel.com>
2024-02-21 18:01:30 +0530

    Update abi_bdba_scan.py

--- abi_bdba_scan.py
+++ abi_bdba_scan.py
@@ -19,10 +19,11 @@ pip_cmd = f"pip install --upgrade pip && pip install  abi==3.0.0 --extra-index-u
 pip_cmd_output = subprocess.run(pip_cmd, capture_output=True, text=True)
 
 print(pip_cmd_output.stdout)
+print(pip_cmd_output.stderr)
 
 pkg_name = sys.argv[2]
 bdba_scan_cmd = f"abi binary_scan scan --timeout 40 --wait --tool_url https://bdba001.icloud.intel.com --tool_group 6  --report_name BDBA_Report --include_components  --zip_file {pkg_name} --username {scan_username} --password {scan_password} --debug "
 bdba_scan_cmd_output = subprocess.run(bdba_scan_cmd, capture_output=True, text=True)
 
 print(bdba_scan_cmd_output.stdout)
-#print(bdba_scan_cmd_output.stderr)
+print(bdba_scan_cmd_output.stderr)

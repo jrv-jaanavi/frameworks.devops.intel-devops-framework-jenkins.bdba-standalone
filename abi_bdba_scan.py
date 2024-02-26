1b878d4c271131fa311702d79c13c885eaddeb91
jaanavi_jrv <jaanavi.rv@intel.com>
2024-02-26 20:36:58 +0530

    Update abi_bdba_scan.py

--- abi_bdba_scan.py
+++ abi_bdba_scan.py
@@ -3,16 +3,16 @@ import sys
 import os
 
 
-# scan_username = os.environ.get("ScanCred_USR")
-# scan_password = os.environ.get("ScanCred_PSW")
+scan_username = os.environ.get("ScanCred_USR")
+scan_password = os.environ.get("ScanCred_PSW")
 
-# artifact_url = sys.argv[1]
-# download_artifact = f"curl -u {scan_username}:{scan_password} -O {artifact_url}"
-# download_artifact_split = download_artifact.split()
-# return_code = subprocess.run(download_artifact_split, capture_output=True, text=True)
+artifact_url = sys.argv[1]
+download_artifact = f"curl -u {scan_username}:{scan_password} -O {artifact_url}"
+download_artifact_split = download_artifact.split()
+return_code = subprocess.run(download_artifact_split, capture_output=True, text=True)
 
-# print(return_code.stdout)
-# print(return_code.stderr)
+print(return_code.stdout)
+print(return_code.stderr)
 
 print("BDBA execution")
 artifact_output = subprocess.run(['ls', '-l'], capture_output=True, text=True)
@@ -20,17 +20,17 @@ artifact_output = subprocess.run(['ls', '-l'], capture_output=True, text=True)
 print(artifact_output.stdout)
 print(artifact_output.stderr)
 
-# pip_cmd = f"pip install  abi==3.0.0 --extra-index-url=https://ubit-artifactory-or.intel.com/artifactory/api/pypi/one-windows-pypi-local/simple --proxy=http://proxy-chain.intel.com:912 "
-# pip_cmd_split = pip_cmd.split()
-# pip_cmd_output = subprocess.run(pip_cmd, capture_output=True, text=True)
+pip_cmd = f"pip install  abi==3.0.0 --extra-index-url=https://ubit-artifactory-or.intel.com/artifactory/api/pypi/one-windows-pypi-local/simple --proxy=http://proxy-chain.intel.com:912 "
+pip_cmd_split = pip_cmd.split()
+pip_cmd_output = subprocess.run(pip_cmd, capture_output=True, text=True)
 
-# print(pip_cmd_output.stdout)
-# print(pip_cmd_output.stderr)
+print(pip_cmd_output.stdout)
+print(pip_cmd_output.stderr)
 
-# pkg_name = sys.argv[2]
-# bdba_scan_cmd = f"abi binary_scan scan --timeout 40 --wait --tool_url https://bdba001.icloud.intel.com --tool_group 6  --report_name BDBA_Report --include_components  --zip_file {pkg_name} --username {scan_username} --password {scan_password} --debug "
-# bdba_scan_cmd_split = bdba_scan_cmd.split()
-# bdba_scan_cmd_output = subprocess.run(bdba_scan_cmd_split, capture_output=True, text=True)
+pkg_name = sys.argv[2]
+bdba_scan_cmd = f"abi binary_scan scan --timeout 40 --wait --tool_url https://bdba001.icloud.intel.com --tool_group 6  --report_name BDBA_Report --include_components  --zip_file {pkg_name} --username {scan_username} --password {scan_password} --debug "
+bdba_scan_cmd_split = bdba_scan_cmd.split()
+bdba_scan_cmd_output = subprocess.run(bdba_scan_cmd_split, capture_output=True, text=True)
 
-# print(bdba_scan_cmd_output.stdout)
-# print(bdba_scan_cmd_output.stderr)
+print(bdba_scan_cmd_output.stdout)
+print(bdba_scan_cmd_output.stderr)

04615dcdcf7d627031f2420d82c47393c600df9d
jaanavi_jrv <jaanavi.rv@intel.com>
2024-03-12 15:16:34 +0530

    Update onekitscan.py

--- onekitscan.py
+++ onekitscan.py
@@ -1,6 +1,6 @@
 import requests
 import sys
-import time
+import datetime
 
 
 file_url = sys.argv[1]
@@ -8,21 +8,28 @@ post_url = "http://onekitscan.idoc.intel.com/api/v1/scan"
 body = { "submitted_user": "jrv", "reference_id": "1234", "file_url": file_url }
 post_response = requests.post(post_url, json=body)
 post_response_json = post_response.json()
+upload_time = datetime.datetime.utcnow()
 print("Response after uploading to BDBA server: ", post_response_json)
 scan_id = post_response_json["id"]
 get_url = f"http://onekitscan.idoc.intel.com/api/v1/scan?id={scan_id}"
-iterations = 1000
+iterations = 5000
 for i in range(iterations):
   get_response = requests.get(get_url)
   get_response_json = get_response.json()
   print("Status of scan: ", get_response_json)
   if get_response_json['status'] == "success":
     break
-  time.sleep(30)
+  time.sleep(20)
   iterations-=1
   if i == iterations - 1:
     print("Retry exceeded")
     break
   print(f"Retrying status check {iterations}:", get_response_json)
+
+scan_complete_time = datetime.datetime.utcnow()
+
+time_taken = (scan_complete_time - upload_time).total_seconds()
+
+print("Time taken for the scan:", time_taken)
     
 

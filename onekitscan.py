fe5aba1eb0e25c027eb9323c60da57f9ee0bbdf2
jaanavi_jrv <jaanavi.rv@intel.com>
2024-03-25 23:19:43 +0530

    Update onekitscan.py

--- onekitscan.py
+++ onekitscan.py
@@ -7,9 +7,9 @@ import time
 file_url = sys.argv[1]
 post_url = "http://onekitscan.idoc.intel.com/api/v1/scan"
 body = { "submitted_user": "jrv", "reference_id": "1234", "file_url": file_url }
+upload_time = datetime.datetime.utcnow()
 post_response = requests.post(post_url, json=body)
 post_response_json = post_response.json()
-upload_time = datetime.datetime.utcnow()
 print("Response after uploading to BDBA server: ", post_response_json)
 scan_id = post_response_json["id"]
 get_url = f"http://onekitscan.idoc.intel.com/api/v1/scan?id={scan_id}"
